8f858531eeba1da82e999fab311b120412948143
Stefan Kaes <stefan.kaes@xing.com>
2016-04-29 20:30:01 +0200

    PROBLEM: ffi-rzmq tests crash with malloc errors
    SOLUTION: change memory layout of class LibZMQ::Message
    
    For some unknown reason, the tests of ffi-rzmq crash when using
    any libzmq version higher than 4.0.7, even though the number of
    bytes of the current declaration is correct for 4.1.1+.
    
    I changed the layout to correspond exactly what zmq.h
    exposes to a C program. This fixed all the crashes of
    the tests and also my programs running on 4.1.4.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require File.expand_path(
 File.join(File.dirname(__FILE__), %w[.. lib ffi-rzmq-core]))
 
 require 'rspec'
-require 'rspec/autorun'
 
 Dir[File.join(File.dirname(__FILE__), "support", "*.rb")].each do |support|
   require support
